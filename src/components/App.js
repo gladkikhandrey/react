96b4f92b93c01420dd17db13beb5235df5dcd711
Andrey Gladkikh <andrgladkikh@gmail.com>
2019-04-06 22:27:53 +0700

    articles added

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Article from './Article';
+import articles from '../fixtures'
+
+function App() {
+	return (
+		<div>
+			<Article article={articles[0]}/>
+		</div>
+	)
+}
+
+export default App
---
+++ src/fixtures.js
@@ -0,0 +1,8 @@
+export default [
+	{
+		"id": "1",
+		"date": "2019-04-06T20:44:23.000Z",
+		"title": "I'm happy",
+		"text": "As I said, I'm very-very happy!"
+	}
+]
